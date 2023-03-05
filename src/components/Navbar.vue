1960e906f0eb3cad3d83145763e4defeebc8e847
דולב נזר <u9248515@bsmch.net>
2023-03-05 16:35:04 +0200

    feat:(NavBar): aliigned items propperly

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,10 +4,6 @@
       <img src="../assets/user-icon.png" class="user-icon" 
       />
       
-      <button> 
-        <img src="../assets/blue-chicken.png" alt="buttonpng" border="0" height="50px"  />
-      </button>
-
       <a href="https://www.youtube.com/watch?v=COfkdOu9KiI">
         <button>
           <img src="../assets/panther-logo.png" alt="buttonpng" border="0" height="50px"  /> </button>
@@ -18,8 +14,12 @@
             <img src="../assets/rhino-logo.png" alt="buttonpng" border="0" height="50px"  />
            </button>
       </a>
+
+      <button> 
+        <img src="../assets/blue-chicken.png" alt="buttonpng" border="0" height="50px"  />
+      </button>
       
-      <h1>Cordon Blue</h1>
+      <h1 class="title">Cordon Bleu</h1>
     </b-navbar>
   </bdi>
 </template>
@@ -28,25 +28,11 @@
 
 export default {
   name: "Navbar",
-  data() {
-    return {
-      locations: [],
-    };
-  },
-  computed: {
-  },
-  async created() {
-  },
-  methods: {
-  },
 };
 </script>
 
 <style scoped>
-.user-icon {
-  height: 4.4vw;
-  float: left;
-}
+
 button {
   background-color: transparent;
   background-repeat: no-repeat;
@@ -54,5 +40,24 @@ button {
   cursor: pointer;
   overflow: hidden;
   outline: none;
+}
+
+button:hover{
+  background-color: lightblue;
+  opacity: 20%;
+  border-radius: 30%;
+}
+
+.user-icon {
+  height: 4.4vw;
+  position: absolute;
+  right: 0;
+  display: block;
+}
+.title{
+  position: absolute;
+  left: 50%;
+  margin-left: -50px !important;
+  display: block;
 }
 </style>
