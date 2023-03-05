e7a36b05bd6aeead249e6c7320411ca8ec681c57
דולב נזר <u9248515@bsmch.net>
2023-03-05 15:59:54 +0200

    feat:(NavBar): added buttons and icons

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,14 @@
 <template>
   <bdi>
     <b-navbar toggleable="lg" type="dark" variant="info">
-      <img src="../assets/user-icon.png" class="user-icon" />
+      <img src="../assets/user-icon.png" class="user-icon" 
+      />
+      <h1>Cordon Blue</h1>
+      <button> 
+        <img src="../assets/blue-chicken.png" alt="buttonpng" border="0" height="50px"  />
+      </button>
+      <button> <img src="../assets/blue-chicken.png" alt="buttonpng" border="0" height="50px"  /> </button>
+      <button> <img src="../assets/blue-chicken.png" alt="buttonpng" border="0" height="50px"  /> </button>
     </b-navbar>
   </bdi>
 </template>
