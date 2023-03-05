2353916bd027047bd2ea9e8be4a6a099409a50b6
eduardStudent <127023949+eduardStudent@users.noreply.github.com>
2023-03-05 20:22:33 +0200

    Revert "Feature filter events"

--- src/main.js
+++ src/main.js
@@ -1,16 +1,4 @@
-import "@babel/polyfill";
-import "mutationobserver-shim";
-import Vue from "vue";
-import "./plugins/bootstrap-vue";
-import App from "./App.vue";
-import store from "./store/store.js";
-import VueAlertify from "vue-alertify";
+import { createApp } from 'vue'
+import App from './App.vue'
 
-Vue.use(VueAlertify);
-
-Vue.config.productionTip = false;
-
-new Vue({
-  store,
-  render: h => h(App)
-}).$mount("#app");
+createApp(App).mount('#app')
