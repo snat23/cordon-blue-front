c966ea30c68a37318379381b8382880bbb0a8aed
דולב נזר <u9248515@bsmch.net>
2023-03-05 15:45:59 +0200

    fix:(NavBar): added user icon and cleaned up NavBar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,31 +1,12 @@
 <template>
   <bdi>
-    <b-navbar toggleable="lg" type="dark" variant="dark">
-      <img src="@/assets/logo.png" class="icon" />
-      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
-      <b-nav-form v-if="selectedLocation">
-        <b-dropdown id="selectLocation" :text="selectedLocation.name">
-          <b-dropdown-item
-            @click="locationSelected(location)"
-            v-for="location in locationsToOptions"
-            :key="location.text"
-          >
-            {{ location.text }}
-          </b-dropdown-item>
-        </b-dropdown>
-      </b-nav-form>
-
-      <b-tooltip target="selectLocation" placement="right">
-        <strong>לחץ </strong> כדי לשנות מיקום
-      </b-tooltip>
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <img src="../assets/user-icon.png" class="user-icon" />
     </b-navbar>
   </bdi>
 </template>
 
 <script>
-import { mapActions, mapState } from "vuex";
-import api from "../api.js";
 
 export default {
   name: "Navbar",
@@ -35,46 +16,17 @@ export default {
     };
   },
   computed: {
-    ...mapState(["selectedLocation"]),
-    locationsToOptions() {
-      return this.locations.map((loc) => {
-        return {
-          value: loc,
-          text: loc.name,
-        };
-      });
-    },
   },
   async created() {
-    await this.loadFromServer();
-    this.updateLocation(this.locations[0]);
   },
   methods: {
-    ...mapActions(["updateLocation"]),
-    locationSelected(location) {
-      this.updateLocation(location.value);
-      this.$alertify.success("המיקום התעדכן");
-    },
-    async loadFromServer() {
-      try {
-        const { data } = await api.locations().getAll();
-        this.locations = data;
-      } catch (err) {
-        this.errorMsg("יש בעייה בטעינת המיקומים");
-      }
-    },
-    errorMsg(msg) {
-      this.$alertify.error(msg);
-    },
-    successMsg(msg) {
-      this.$alertify.success(msg);
-    },
   },
 };
 </script>
 
 <style scoped>
-.icon {
+.user-icon {
   height: 4.4vw;
+  float: left;
 }
 </style>
