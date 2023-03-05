a917210051f0c56cf3d288846e518fc69ed12db1
ליהיא פנירי <u9231363@bsmch.net>
2023-03-05 20:56:24 +0200

    fix dev

--- src/App.vue
+++ src/App.vue
@@ -9,8 +9,9 @@
 import NavBar from "./components/Navbar.vue";
 import Sidebar from "./components/FloatingSidebar.vue"
 
+
 export default {
-  name: 'App',
+  name: "App",
   components: {
     NavBar,
     Sidebar
@@ -19,12 +20,13 @@ export default {
 </script>
 
 <style>
+@import url("https://fonts.googleapis.com/css?family=Heebo");
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Heebo";
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
