dcae6bf8e0048a6707ce4b102f8f6572eb29d1dd
אראל מאור <u9199290@bsmch.net>
2023-03-06 17:26:04 +0200

    צfix(MapComponent.vue): fixed map

--- src/components/MapComponent.vue
+++ src/components/MapComponent.vue
@@ -1,10 +1,11 @@
 <template>
-  <l-map style="height: 400px" :zoom="zoom" :center="center">
+  <l-map style="height: 400px" :zoom="zoom" :center="center" @click="getLocation">
     <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
     <l-marker 
     v-for="(event, index) in events"
     :key="index"
     :lat-lng="event"
+    :icon="icon"
     ></l-marker>
   </l-map>
 </template>
@@ -13,7 +14,7 @@
 import { latLng } from "leaflet";
 
 import { LMap, LTileLayer, LMarker } from "vue2-leaflet";
-
+import dangerIcon from "../assets/danger.png"
 export default {
   name: "main-map",
   components: {
@@ -26,10 +27,21 @@ export default {
       url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
       attribution:
         '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-      zoom: 9,
+      zoom: 10,
       center: latLng(32.0619, 35.1124),
       events: [[32.0619, 35.1124], [32.1203, 35.1402], [32.1223, 35.1202]],
+      icon: L.icon({
+        iconUrl: dangerIcon,
+        iconSize: [22, 22],
+        iconAnchor: [16, 37]
+      }),
     };
   },
+  methods: {
+    getLocation(event) {
+    console.log([event.latlng.lat, event.latlng.lng])
+    return [event.latlng.lat, event.latlng.lng];
+  }
+  }
 };
 </script>
