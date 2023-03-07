dddd0d889117702c95d156628912c72bf487db37
ליאור פדידה <u9154613@bsmch.net>
2023-03-07 17:36:03 +0200

    add login and home page

--- src/main.js
+++ src/main.js
@@ -6,6 +6,8 @@ import App from "./App.vue";
 import store from "./store/store.js";
 import VueAlertify from "vue-alertify";
 import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+import router from './router/routes'
+
 
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
@@ -13,12 +15,11 @@ import "leaflet/dist/leaflet.css";
 
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
-
 Vue.use(VueAlertify);
-
 Vue.config.productionTip = false;
 
 new Vue({
   store,
+  router,
   render: h => h(App)
 }).$mount("#app");
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -6,15 +6,44 @@
                 <h1 class="text-center">כניסה</h1>
                 <div class="input-group  my-3">
                     <i class="bi bi-person-fill input-group-text"></i>
-                    <input type="text" id="accountNum" class="form-control" placeholder="מספר חשבון">
+                    <input v-model="username" type="text" id="accountNum" class="form-control" placeholder="מספר חשבון">
                 </div>
                 <div class="input-group  mb-3">
                     <i class="bi bi-lock-fill input-group-text"></i>
-                    <input type="text" id="password" class="form-control" placeholder="סיסמה">
+                    <input v-model="password" type="text" id="password" class="form-control" placeholder="סיסמה">
                 </div>
-                <button class="btn btn-primary" id="login" type="button">כניסה</button>
+                <button @click="checkUserValidation()" class="btn btn-primary" id="login" type="button">כניסה</button>
             </form>
         </div>
     </body>
 </template>
 
+<script>
+import api from '../../api/api';
+export default {
+  name: "Login",
+  data() {
+    return {
+      allUsers: [],
+      username: "",
+      password: ""
+    }
+  },
+  async created() {   
+    this.allUsers =  (await api.users().getUsers()).data; 
+    console.log(this.allUsers) 
+  },
+  methods: {
+    checkUserValidation() {
+        
+        if(this.allUsers.find((user) => user.username === this.username &&
+        user.password === this.password)){
+            alert("good")
+            this.$router.push('/home') 
+        } else{
+            alert("couldnt connect")
+        }
+    }
+  }
+};
+</script>
