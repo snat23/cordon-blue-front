cc3f2dabd6b08dcfda70483dfbdcb9cd15a7cb04
ליאור פדידה <u9154613@bsmch.net>
2023-03-08 20:10:53 +0200

    add new favicon to the website

--- src/components/MapComponent.vue
+++ src/components/MapComponent.vue
@@ -89,7 +89,7 @@ export default {
     getLocation(event) {
       this.circle = {
         center: [event.latlng.lat, event.latlng.lng],
-        radius: 200,
+        radius: 130,
         color: "red",
       };
       this.isCurrent = true;
