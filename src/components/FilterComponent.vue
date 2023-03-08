17e750a1fdecac91691a3bb27e5fcbc9f1b7f38d
עדי  ארונוביץ <u9063218@bsmch.net>
2023-03-08 20:50:52 +0200

    feat(FilterComponent):add filter events by location and dates

--- src/components/FilterComponent.vue
+++ src/components/FilterComponent.vue
@@ -1,5 +1,6 @@
 <template>
-  <div>
+<div>
+  <div v-if="showFilter">
     <label for="example-input">בחר תאריך</label>
     <b-input-group class="mb-3">
       <b-form-input
@@ -29,15 +30,24 @@
       ></b-form-select>
     </b-input-group>
 
-    <label>מיקום:</label>
+    <label>מיקום X:</label>
     <b-input-group>
       <b-form-input
         id="input-2"
-        v-model="place"
-        placeholder="Enter name"
+        v-model="placeX"
         required
       ></b-form-input>
     </b-input-group>
+
+    <label>מיקום Y:</label>
+    <b-input-group>
+      <b-form-input
+        id="input-2"
+        v-model="placeY"
+        required
+      ></b-form-input>
+    </b-input-group>
+
     <label>אמל''ח</label>
     <b-input-group>
       <b-form-select
@@ -53,15 +63,27 @@
       >סנן</b-button
     >
   </div>
+  <div v-else > 
+    <div> <b-button
+      @click="backToFilter"
+      variant="primary"
+      class="but"
+      >חזור לסינון</b-button
+    >
+    <h1>אירועים לאחר סינון </h1></div>
+      <last-events :lastEvents="currentFilteredEvents"  class="col-12 mt-3 text-center" id="lastEventsCom"/>
+  </div>
+</div>
 </template>
 
 <script>
 import api from "../../api/api.js";
-
+import LastEvents from "./LastEvents.vue";
 export default {
   data() {
     return {
-      place: null,
+      placeX: null,
+      placeY: null,
       date: null,
       formatted: "",
       selected: "",
@@ -70,7 +92,12 @@ export default {
       eventTypes: null,
       weaponTypes: null,
       events: [],
+      showFilter: true,
+      currentFilteredEvents: [],
     };
+  },
+   components: {
+    LastEvents,
   },
   props: {
     filterEventsList: Array,
@@ -88,23 +115,43 @@ export default {
       this.selected = ctx.selectedYMD;
     },
     async getFilteredArray() {
-
+      let place = [];
+      if(this.placeX!= null && this.placeY!= null){
+        place = [this.placeX, this.placeY];
+      } else {
+        place = null;
+      }
+      
       this.selectedWeapon !== null ? this.selectedWeapon = (await api.weapons().getWeaponByName(this.selectedWeapon)).data : null;
       this.selectedEvent !== null ? this.selectedEvent = (await api.eventTypes().getEventTypeByName(this.selectedEvent)).data : null;
-      const dateSelected = new Date(this.date);
+      let dateSelected = null;   
       let endOfDaySelected =  new Date();
-      endOfDaySelected.setDate(dateSelected.getDate() + 1);
+      if(this.date!== null) {
+        dateSelected = new Date(this.date);
+        endOfDaySelected.setDate(dateSelected.getDate() + 1);
+      }
       const filteredEvents = this.filterEventsList.filter((event) => {
-        const eventDate = new Date(event.time);
-        console.log(((dateSelected <= eventDate) && (eventDate < endOfDaySelected)));
-      return (((this.place === event.place) || (this.place === null)) && 
+       const eventDate = new Date(event.time);
+      return (((place === null))||(place.toString() === event.coordinates.toString())  && 
       (((dateSelected <= eventDate) && (eventDate < endOfDaySelected)) || (this.date === null))&&
       ((this.selectedWeapon === null) || (this.selectedWeapon.weaponId === event.weapon)) && 
       ((this.selectedEvent === null) || (this.selectedEvent.eventId === event.eventType)))
       });
+      this.showFilter=false;
+      this.currentFilteredEvents = filteredEvents;
 
-      console.log(filteredEvents);
-      return this.events;
+    },
+    backToFilter() {
+      this.showFilter=true;
+      this.placeX = null,
+      this.placeY = null,
+      this.date = null,
+      this.formatted = "",
+      this.selected = "",
+      this.selectedEvent = null,
+      this.selectedWeapon = null,
+      this.events = [],
+      this.currentFilteredEvents = []
     },
   },
 };
@@ -116,5 +163,9 @@ export default {
   color: black;
   background-color: #a1cfed;
   border-color: #84bee6;
+}
+#lastEventsCom {
+  width: 450px;
+  margin-left: -100px;
 }
 </style>
